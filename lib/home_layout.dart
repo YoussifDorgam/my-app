1d50280ec120e509952266e5f6d7746957564263
youssif <yosefdor80@gmail.com>
2021-12-31 01:28:44 +0200

    my app

---
+++ lib/home_layout.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:my_broject/product_model.dart';
+import 'package:my_broject/story_screen.dart';
+
+class HomeLayout extends StatelessWidget {
+  const HomeLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(2500),
+        )),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(150),
+          child: Padding(
+              padding: const EdgeInsets.fromLTRB(12, 0, 2, 70),
+              child: Row(children: const [
+                Text(
+                  'Welcome bro ',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 27,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(5, 10, 0, 5),
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                    )),
+              ])),
+        ),
+      ),
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:
+          [
+          const Padding(
+            padding: EdgeInsetsDirectional.only(top: 10),
+            child: Text(' list view' ,
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontSize: 30 ,
+            ),),
+          ),
+          const SizedBox(),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            itemCount: products.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, childAspectRatio: 0.80),
+            itemBuilder: (context, index) => CategoryCard(
+              product: products[index],
+            )),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+  final Product product;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: InkWell(
+        onTap: ()
+        {
+          if(product.id == 1 ) {Navigator.of(context).push(MaterialPageRoute(builder: (context) => const StoryScreen(),));
+          }else{
+
+          }
+        },
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              color: product.color, borderRadius: BorderRadius.circular(15.0)),
+          child: Column(
+            children: [
+              Image.asset(
+                product.image,
+                height: 100,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                product.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "${product.courses} Story",
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'home_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Directionality(
+          textDirection: TextDirection.ltr,
+          child: HomeLayout()),
+    );
+  }
+}
